bca38cc5f777dc4f9e4dc8c9cdf2ef5ac14dcdf7
Ritika <Ritikaverma3147@gmail.com>
2023-09-05 18:44:30 +0530

    first commit

---
+++ responsive-portfolio-main/js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi,</span> I am currently a 4th-year undergraduate in the Computer Science And Engineering at Noida Institute of Engineering And Technology, Greater Noida . As a Computer Science Student, I enjoy tackling new challenges and continuously expanding my skillset. I am proficient in Java as well as have knowledge of MERN  I have a passion for working with Node.js, MongoDB, and  Javascript libraries and frameworks  like React.js, I am also interested in building new Web Technologies and Products."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
